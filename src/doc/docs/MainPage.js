49bec2da4bc22dcca26668878d5e1da7114b66b0
jujucom@126.com <jujucom@126.com>
2019-04-11 22:19:54 +0800

    local change

--- src/doc/docs/MainPage.js
+++ src/doc/docs/MainPage.js
@@ -3,24 +3,45 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
 export default class MainPage extends Component {
   render() {
-    return <div>
+    return (<div>
       <div className="row">
-        <div className="col-xs-12 col-sm-4">
-          <div className="menu simple">
+        <div className="col-xs-12 col-sm-3 col-md-2" >
+
+          <div className="simple menu" style={{width: "100%", height: "calc(100vh - 4rem)"}}>
             <div className="submenu">
-              <div className="menu-header">快速开始</div>
-              <div className="menu primary">
-                <li className="item">安装</li>
-                <li className="item active">基础</li>
+              <div className="menu-header">Geting Started</div>
+              <div className="menu-list">
+                <div className="item">
+                  Install
+                </div>
+                <div className="item">
+                  Basic
+                </div>
+                <div className="submenu">
+                  <div className="menu-header">
+                    Components
+                  </div>
+                  <div className="menu-list">
+                    <div className="item">
+                      Button
+                    </div>
+                    <div className="item">
+                      Input
+                    </div>
+                  </div>
+                </div>
+                <div className="item">
+                  Layout
+                </div>
               </div>
             </div>
-
           </div>
+
         </div>
-        <div className="col-xs-12 col-sm-8"></div>
+        <div className="col-xs-12 col-sm-9 col-md-10"></div>
 
       </div>
-    </div>;
-  }
+      < /div>);
 
-}
+        }
+        }
