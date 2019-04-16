f4d9ad2ed47d09857f74c4abc53ac6b898ed7b7b
Z_J_WANG <z.j.wang@oracle.com>
2019-04-16 18:12:51 +0800

    update

--- src/doc/docs/MainPage.js
+++ src/doc/docs/MainPage.js
@@ -3,13 +3,12 @@ import React, {Component} from "react";
 export default class MainPage extends Component {
   render() {
     return (<div>
-      <div className="row">
-        <div className="doc left-menu-fixed col-xs-12 col-sm-3 col-md-2">
-
+      <div className="doc grid-row row">
+        <div className="doc left-nav-col col-md-2">
           <div className="simple menu"
                style={{width: "100%", height: "calc(100vh - 4rem)"}}>
             <div className="submenu">
-              <div className="menu-header">Geting Started</div>
+              <div className="menu-header">Getting Started</div>
               <div className="menu-list">
                 <div className="item">
                   Install
@@ -36,16 +35,20 @@ export default class MainPage extends Component {
               </div>
             </div>
           </div>
+        </div>
+        <div className="doc center-col col-md-8">
+          <div style={{height: "300px"}}>d</div>
+          <div style={{height: "300px"}}>d</div>
+          <div style={{height: "300px"}}>d</div>
+          <div style={{height: "300px"}}>d</div>
+          <div style={{height: "300px"}}>d</div>
+        </div>
+        <div className="doc right-col col-md-2">
 
         </div>
-        <div className="col-xs-12 col-sm-9 col-md-10">
-          <div style={{height: "300px"}}>d</div>
-          <div style={{height: "300px"}}>d</div>
-          <div style={{height: "300px"}}>d</div>
-          <div style={{height: "300px"}}>d</div>
-          <div style={{height: "300px"}}>d</div>
-        </div>
+
       </div>
+
       < /div>);
 
         }
